2260d87c56870e3d7d0900103a285569e7c92eb3
unknown <ereteyey@naver.com>
2025-03-20 19:10:32 +0900

    Postcss, autoprefixer, tailwind설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import Bootstrap from './pages/Bootstrap'
-import Icon from './pages/Icon'
-import Style from './pages/Style'
-import UsingIcon from './pages/UsingIcon'
-import UsingIconWithCSSClass from './pages/UsingIconWithCSSClass'
+import Tailwindcss from './pages/Tailwindcss'
+import Color from './pages/Color'
+import TextsTest from './pages/TextsTest'
 import './App.css'
 
 export default function App() {
   return (
     <div>
-      <UsingIconWithCSSClass />
+      <TextsTest />
       {/* 주석 */}
-      <UsingIcon />
-      <Style />
-      <Icon />
-      <Bootstrap />
+      <Color />
+      <Tailwindcss />
     </div>
   )
 }
