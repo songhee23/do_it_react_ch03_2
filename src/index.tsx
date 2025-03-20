c37852fadab5bc700ed604112a5eb484736e19d1
unknown <ereteyey@naver.com>
2025-03-20 20:30:48 +0900

    테일윈드CSS 테스트 코드 작성

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@fontsource/material-icons';
-import Icon from './pages/Icon';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  safelist: [{pattern: /^line-clamp-(\d+)$/}],
+  /*
+  -> line-clamp로 시작하는 클래스 이름을 
+  동적으로 조합하더라고 정상적으로 동작하도록 
+  트리쉐이킹 대상에서 제거하는 코드..?
+  */
+  plugins: [require('@tailwindcss/line-clamp'), require('daisyui')],
 }
 
