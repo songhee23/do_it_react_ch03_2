e4b285860b6fd6bd61baa6c08646c2a38f75aedf
unknown <ereteyey@naver.com>
2025-03-20 20:55:20 +0900

    텍스트 관련 컴포넌트 구현

--- src/pages/Color.tsx
+++ src/pages/Color.tsx
@@ -1,3 +1,14 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>
+export default function Color() {
+  return (
+    <div className="p-4 bg-sky-700">
+      {/*  */}
+      <p className="w-full p-4 text-3xl text-white">Tailwindcss</p>
+      <div className="mb-4">
+        <p className="text-white">Email address</p>
+        {/*  */}
+        <input type="email" className="text-gray-900 border-sky-200 botder-4"/>
+        <p className="text-rose-500">This field is required</p>
+      </div>
+    </div>
+  )
 }
--- src/pages/Tailwindcss.tsx
+++ src/pages/Tailwindcss.tsx
@@ -3,8 +3,10 @@ import * as D from '../data'
 export default function Tailwindcss() {
   return (
     <div className='bg-black/70'>
+      {/* 불투명도 70% */}
       <p className="w-full p-4 text-3xl text-white">Tailwindcss</p>
       <p className='italic text-gray-50 line-clamp-3'>{D.randomParagraphs(10)}</p>
+      {/* line-clamp-3 텍스트가 아무리 길어도 3줄을 넘지 말라는 뜻 */}
       <button className="btn btn-primary" style={{textTransform: 'none'}}>
         Button
       </button>
